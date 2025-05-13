7ef20bf8782580cb35b459877e543d4f99a9d44d
John97091065 <97091065@st.deltion.nl>
2025-05-13 15:20:27 +0200

    frontend stuff

---
+++ src/pages/admin/Register.jsx
@@ -0,0 +1,11 @@
+import RegisterAdForm from "../../components/RegisterAdForm";
+
+function RegisterAdPage() {
+    return (
+        <div className="container">
+        <h1>Register Admin</h1>
+        <RegisterAdForm />
+        </div>
+    );
+}
+export default RegisterAdPage;
